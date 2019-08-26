730a6c59b586bac66f8103d689474e915d1f457f
saguiguilid <tbki.jea@gmail.com>
2019-08-26 21:00:06 +0800

    Modularize the React application

--- sports-legend/src/App.js
+++ sports-legend/src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ({ games }) => (
+  <ul>
+    {games.map(game => (
+      <li key={game.id}>
+        {game.name} ({game.personality})
+      </li>
+    ))}
+  </ul>
+);
 
 export default App;
--- sports-legend/src/index.js
+++ sports-legend/src/index.js
@@ -4,7 +4,14 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const gameList = [
+  { id: 1, name: "Basketball", personality: "Michael Jordan" },
+  { id: 2, name: "Billiard", personality: "Efren Reyes" },
+  { id: 3, name: "Golf", personality: "Tiger Woods" },
+  { id: 4, name: "Volleyball", personality: "Leila Barros" }
+];
+
+ReactDOM.render(<App games={gameList} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
